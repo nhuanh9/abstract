645de3a1e01c67adb49a88bd85004b086f993b89
nhuanh9 <chopperlouis@gmail.com>
2019-07-08 08:54:59 +0700

    .

---
+++ src/main.java
@@ -0,0 +1,14 @@
+import animal.Animal;
+import animal.Chicken;
+import animal.Tiger;
+
+public class main {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        for (Animal animal : animals) {
+            animal.makeSound();
+        }
+    }
+}
